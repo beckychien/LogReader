bcd0a9e0b9e522be870fdc74fe2bf18f93ff377e
Becky <becky6110@gmail.com>
2021-07-05 15:59:15 +0800

    Update newlog_upload.py

--- newlog_upload.py
+++ newlog_upload.py
@@ -5,7 +5,7 @@ def test_uploads():
 
 	file = '/Users/fox/Desktop/newlog/73-17637-11-180909051904-FCH2235711V'
 
-	url = "http://10.132.46.138:5002/newlog"
+	url = "http://127.0.0.1:5002/newlog"
    
 	files = {'file':open(file,'rb')}
 
