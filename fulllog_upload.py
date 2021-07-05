71f64da2221696808efb90dbd46656a71f53bc20
Becky <becky6110@gmail.com>
2021-07-05 15:59:02 +0800

    Update fulllog_upload.py

--- fulllog_upload.py
+++ fulllog_upload.py
@@ -5,7 +5,7 @@ def test_uploads():
 
 	file = '/Users/fox/Desktop/fulllog/FOC22401H1B181009163824.txt'
 
-	url = "http://10.132.46.138:5002/fullog"
+	url = "http://127.0.0.1:5002/fullog"
    
 	files = {'file':open(file,'rb')}
 
